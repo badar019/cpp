583940e59f9c95ba01d5dd69ef074b731ba65346
Badar Uddin <mdbadaruddin019@gmail.com>
2025-04-18 21:57:05 +0600

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << "Badar Uddin" << endl;
+    int a;
+    cout <<"Enter a number: ";
+    cin>>a;
+    cout<<5*a<<endl;
+    return 0;
+}
